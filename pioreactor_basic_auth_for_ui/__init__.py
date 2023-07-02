a031468e9cf2a44ab12ed5560353258179fb3d4e
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2023-07-02 17:02:38 -0400

    first commit

---
+++ pioreactor_basic_auth_for_ui/__init__.py
@@ -0,0 +1,22 @@
+import click
+from pioreactor.mureq import basic_auth
+import subprocess
+
+@click.command(name="change_ui_credentials")
+@click.argument("username")
+@click.argument("password")
+def click_change_ui_credentials(username: str, password: str):
+    """
+    (leader only) Change the basic-auth creds for the UI
+    """
+    from shlex import quote
+
+    # write to password file
+    subprocess.run(f"sudo htpasswd -b -c /etc/lighttpd.user.htpasswd {quote(username)} {quote(password)}", shell=True)
+
+    # reboot lighttp server
+    subprocess.run("sudo service lighttpd force-reload", shell=True)
+    # print API key
+    click.echo("Assign the following to field api_key under [ui_basic_auth] in your config.ini:")
+    click.secho(basic_auth(username, password), bold=True)
+
---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="pioreactor-basic-auth-for-ui",
+    version="0.0.1",
+    license_files=("LICENSE.txt",),
+    description="Adding basic auth to the Pioreactor UI",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author_email="hello@pioreactor.com",
+    author="Cameron Davidson-Pilon",
+    url="",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={"pioreactor.plugins": "pioreactor_basic_auth_for_ui = pioreactor_basic_auth_for_ui"},
+)
